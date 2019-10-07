17e39953d5ccfedd209d56121106eccb1ef01f3b
wspalding <thisisspalding@gmail.com>
2019-10-07 14:57:46 -0700

    feels late to call this initial commit

--- RainBowWalls2/GameScene.swift
+++ RainBowWalls2/GameScene.swift
@@ -9,81 +9,190 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+class GameScene: SKScene, SKPhysicsContactDelegate
+{
+    var isGameOver = true
+    var addWallOnTouch = false
+    let player = SKSpriteNode(imageNamed: "dot3")
+    var wallList = [SKSpriteNode]()
+    let rainbow_images = ["testWallRed", "testWallBlue","testWallGreen","testWallYellow","testWallPink","testWallPurple","testWallDarkPurple","testWallOrange","testWallTeal","testWallLightBlue", "testWallGreen2"]
+    var texture = "rainbow"
+    //var playerImage = "dot3"
+    var wallImage : String
+    {
+        var currentTexture : [String]
+        switch texture {
+        case "rainbow":
+            currentTexture = rainbow_images
+            break
+        default:
+            currentTexture = rainbow_images
+            break
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        let randomindex = Int(arc4random()) % currentTexture.count
+        return currentTexture[randomindex]
+    }
+    let remove = SKAction.removeFromParent()
+    let waitThenRemove = SKAction.sequence([ SKAction.wait(forDuration: 5),
+                                             SKAction.removeFromParent()])
+    var difficulty = 1.0
+    struct PhysicsCategory
+    {
+        static let None      : UInt32 = 0
+        static let All       : UInt32 = UInt32.max
+        static let player    : UInt32 = 0b1       // 1
+        static let wall      : UInt32 = 0b10      // 2
+    }
+    
+    
+    override func didMove(to view: SKView)
+    {
+        backgroundColor = SKColor.black
+        physicsWorld.gravity = CGVector.zero
+        physicsWorld.contactDelegate = self
+        player.position = CGPoint(x: frame.size.width * 0.5, y: frame.size.height * 0.5)
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2)
+        player.physicsBody?.categoryBitMask = PhysicsCategory.player
+        player.physicsBody?.contactTestBitMask = PhysicsCategory.All
+        player.physicsBody?.collisionBitMask = PhysicsCategory.All
+        addChild(player)
+    }
+    
+    func gameOver()
+    {
+        clearWalls()
+        isGameOver = true
+        player.removeFromParent()
+        player.position = CGPoint(x: frame.size.width * 0.5, y: frame.size.height * 0.5)
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2)
+        player.physicsBody?.categoryBitMask = PhysicsCategory.player
+        player.physicsBody?.contactTestBitMask = PhysicsCategory.All
+        player.physicsBody?.collisionBitMask = PhysicsCategory.All
+        addChild(player)
+
+    }
+    //MARK: touch functions
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        if(!isGameOver && addWallOnTouch)
+        {
+            guard let touch = touches.first else {return}
+            let touchLocation = touch.location(in: self)
+            spawnStaticWall(at: touchLocation)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+    
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        if(!isGameOver)
+        {
+            guard let touch = touches.first else {return}
+            let touchLocation = touch.location(in: self)
+            player.physicsBody?.velocity = CGVector(dx: touchLocation.x - player.position.x,
+                                                dy: touchLocation.y - player.position.y)
+        }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+    
+    }
+    
+    
+    func didBegin(_ contact: SKPhysicsContact)
+    {
+        //print("contact")
+        if (contact.bodyA.categoryBitMask & contact.bodyB.categoryBitMask == 0 )
+        {
+            gameOver()
         }
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+    //var attackDirection = Double(Int(arc4random()) % 4) + 1
+    var currentAttackDirection = Double(Int(arc4random()) % 4) + 1
+    var attackPosition : CGPoint
+    {
+        switch currentAttackDirection
+        {
+        case 1:
+            return CGPoint(x: player.position.x, y: 11) //top
+        case 2:
+            return CGPoint(x: player.position.x, y: frame.size.height - 11) //bottom
+        case 3:
+            return CGPoint(x: 11, y: player.position.y) //left
+        default:
+            return CGPoint(x: frame.size.width - 11, y: player.position.y) //right
         }
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
+    override func update(_ currentTime: TimeInterval)
+    {
         // Called before each frame is rendered
+        for wall in wallList
+        {
+            if(!frame.intersects(wall.frame))
+            {
+                wall.removeFromParent()
+            }
+        }
+        if(!frame.intersects(player.frame))
+        {
+            gameOver()
+        }
+        if(!isGameOver)
+        {
+            let timeToAttack = (Double(Int(currentTime * difficulty) % 4) + 1)
+            //print("time ", time )
+            //print("direction ", currentAttackDirection)
+            if(timeToAttack == currentAttackDirection)
+            {
+                spawnAttackWall(atPoint: attackPosition, towardsSpriteNode: player)
+                currentAttackDirection = Double(Int(arc4random()) % 4) + 1
+            }
+        }
     }
+    
+    func spawnAttackWall(atPoint: CGPoint, towardsSpriteNode: SKSpriteNode)
+    {
+        let newWall = SKSpriteNode(imageNamed: wallImage)
+        newWall.position = atPoint
+        wallList.append(newWall)
+        newWall.physicsBody = SKPhysicsBody(rectangleOf: newWall.size)
+        newWall.physicsBody?.categoryBitMask = PhysicsCategory.wall
+        newWall.physicsBody?.contactTestBitMask = PhysicsCategory.wall
+        newWall.physicsBody?.collisionBitMask = PhysicsCategory.All
+        addChild(newWall)
+        newWall.physicsBody?.velocity = CGVector(dx: towardsSpriteNode.position.x - newWall.position.x,
+                                                 dy: towardsSpriteNode.position.y - newWall.position.y)
+    }
+    func spawnStaticWall(at: CGPoint)
+    {
+        let wall = SKSpriteNode(imageNamed: wallImage)
+        wallList.append(wall)
+        wall.position = at
+        wall.physicsBody = SKPhysicsBody(rectangleOf: wall.size)
+        wall.physicsBody?.categoryBitMask = PhysicsCategory.wall
+        wall.physicsBody?.contactTestBitMask = PhysicsCategory.wall
+        wall.physicsBody?.collisionBitMask = PhysicsCategory.All
+        addChild(wall)
+        wall.run(waitThenRemove)
+        
+    }
+
+    func clearWalls()
+    {
+        for wall in wallList
+        {
+            wall.removeFromParent()
+        }
+    }
+    
+    
 }
--- RainBowWalls2/GameViewController.swift
+++ RainBowWalls2/GameViewController.swift
@@ -10,46 +10,185 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
+class GameViewController: UIViewController
+{
+    var sceneInstance = GameScene()
+    
+    @IBOutlet weak var spacelabel: UILabel!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+        sceneInstance = scene
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        difficultyLabel.text = difficultyText
+        hightScoreLabel.text = highScoreText
+        spacelabel.isHidden = true
     }
 
-    override var shouldAutorotate: Bool {
+    //@IBOutlet weak var clearButton: UIButton!
+    //@IBAction func clearButtonPressed(_ sender: UIButton)
+    //{
+        //sceneInstance.clearWalls()
+    //}
+    @IBOutlet weak var startButton: UIButton!
+    @IBAction func startButtonPressed(_ sender: UIButton)
+    {
+        startButton.isHidden = true
+        //clearButton.isHidden = true
+        difficultyLabel.isHidden = true
+        incramentDifficultyLabel.isHidden = true
+        upButton.isHidden = true
+        downButton.isHidden = true
+        hightScoreLabel.isHidden = true
+        
+        sceneInstance.isGameOver = false
+        startTimer()
+    }
+    
+    //an array for each difficulty
+    var highScoreList = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],
+                         [0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+    var difficultyText : String
+    {
+        return " \(difficultyNames[Int(sceneInstance.difficulty)]) "
+    }
+    var highScoreText : String
+    {
+        var text = "High Scores: \n"
+        var i = 0
+        while(i < 5)
+        {
+            text += "\(highScoreList[Int(sceneInstance.difficulty)][i]) "
+            if(i < 4)
+            {
+                text += "\n"
+            }
+            i += 1
+        }
+        return text
+    }
+    
+    func updateHighScore()
+    {
+        if(seconds > highScoreList[Int(sceneInstance.difficulty)][4])
+        {
+            let temp = seconds
+            var i = 0
+            while(i < 5)
+            {
+                if(seconds > highScoreList[Int(sceneInstance.difficulty)][i])
+                {
+                    let temp2 = highScoreList[Int(sceneInstance.difficulty)][i]
+                    highScoreList[Int(sceneInstance.difficulty)][i] = seconds
+                    seconds = temp2
+                }
+                i += 1
+            }
+            seconds = temp
+        }
+        UserDefaults.standard.set(highScoreList, forKey: "myHighscores")
+    }
+    
+    @IBOutlet weak var hightScoreLabel: UILabel!
+    @IBAction func difficultyUp(_ sender: UIButton)
+    {
+        if(sceneInstance.difficulty >= 0 && sceneInstance.difficulty < 10)
+        {
+            sceneInstance.difficulty += 1
+            difficultyLabel.text = difficultyText
+            hightScoreLabel.text = highScoreText
+        }
+    }
+    @IBAction func difficultyDown(_ sender: UIButton)
+    {
+        if(sceneInstance.difficulty > 0 && sceneInstance.difficulty <= 10)
+        {
+            sceneInstance.difficulty -= 1
+            difficultyLabel.text = difficultyText
+            hightScoreLabel.text = highScoreText
+        }
+    }
+    @IBOutlet weak var difficultyLabel: UILabel!
+    let difficultyNames = ["nothing", "easy" , "less easy", "not easy" , "medium" ,
+                           "not hard" , "hard" , "harder" , "very hard" ,"even harder" , "don't be bad"]
+    @IBOutlet weak var incramentDifficultyLabel: UILabel!
+    @IBOutlet weak var upButton: UIButton!
+    @IBOutlet weak var downButton: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
+    var seconds = 0
+    var timer = Timer()
+    func startTimer()
+    {
+        seconds = 0
+        timerLabel.text = "Time: \(seconds)"
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(GameViewController.countUp), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer, forMode: RunLoopMode.commonModes)
+    }
+    @objc func countUp()
+    {
+        if(sceneInstance.isGameOver)
+        {
+            stopTimer()
+            startButton.isHidden = false
+            //clearButton.isHidden = false
+            difficultyLabel.isHidden = false
+            incramentDifficultyLabel.isHidden = false
+            upButton.isHidden = false
+            downButton.isHidden = false
+            hightScoreLabel.isHidden = false
+            updateHighScore()
+            hightScoreLabel.text = highScoreText
+        }
+        else
+        {
+            seconds += 1
+            timerLabel.text = "Time: \(seconds)"
+        }
+    }
+    func stopTimer()
+    {
+        timer.invalidate()
+        //TimerLabel.text = ""
+    }
+    
+    override func viewDidAppear(_ animated: Bool) //load highscores
+    {
+        if let highScores = UserDefaults.standard.object(forKey: "myHighscores") as? [[Int]]
+        {
+            highScoreList = highScores
+        }
+    }
+    
+    
+    
+    override var shouldAutorotate: Bool
+    {
         return true
     }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask
+    {
+        if UIDevice.current.userInterfaceIdiom == .phone
+        {
             return .allButUpsideDown
-        } else {
-            return .all
-        }
+        } else {return .all}
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
 
-    override var prefersStatusBarHidden: Bool {
+    override var prefersStatusBarHidden: Bool
+    {
         return true
     }
 }
